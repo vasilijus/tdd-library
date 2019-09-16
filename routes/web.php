a5e2fb95aeb2e385050ab75352c0478d4842b10b
Sergej <sergej@computerassistance.co.uk>
2019-09-16 17:10:07 +0100

    route setup

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,7 @@ Route::get('/', function () {
 });
 
 Route::post('/books','BooksController@store');
-
 Route::patch('/books/{book}','BooksController@update');
+Route::delete('/books/{book}', 'BooksController@destroy');
+
+Route::post('/author' , 'AuthorController@store');
